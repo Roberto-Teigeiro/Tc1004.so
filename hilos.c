fc8c53041fcba75be4093d6e965d75ec6034e6ae
Roberto Teigeiro <a01643651@tec.mx>
2023-10-10 08:25:48 -0600

    hilo

--- hilos.c
+++ hilos.c
@@ -7,7 +7,10 @@ printf("Hola desde un hilo\n");
 
 int main(){
     pthread_t t;
+    for(int i=0;i>=100;i++){
     pthread_create(&t,NULL,holahilo,NULL);
+    
+    }
     pthread_exit(NULL);
     return 0;
 }
